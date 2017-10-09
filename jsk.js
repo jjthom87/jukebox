b285468132f71fb9efc75d5af46669ccd7031ad1
Jared Thomas <jjthom87@yahoo.com>
2017-10-08 23:43:42 -0400

    got click on song to work

--- jsk.js
+++ jsk.js
@@ -2,42 +2,52 @@ $(document).ready(function(){
 
 
 	var songs = [{
+		id: 1,
 		song: 'new_wave',
 		screen: 'New Wave', 
 		artist: 'Against Me!'  
 	},{
+		id: 2,
 		song: 'stop', 
 		screen: 'Stop',
 		artist: 'Against Me!' 
 	},{
+		id: 3,
 		song: 'thrash_unreal',
 		screen: 'Thrash Unreal',
 		artist: 'Against Me!'
 	},{
+		id: 4,
 		song: 'up_the_cuts',
 		screen: 'Up The Cuts',
 		artist: 'Against Me!'
 	},{
+		id: 5,
 		song: 'basket_case',
 		screen: 'Basket Case',
 		artist: 'Green Day'
 	},{
+		id: 6,
 		song: 'brain_stew',
 		screen: 'Brain Stew',
 		artist: 'Green Day'
 	},{
+		id: 7,
 		song: 'she',
 		screen: 'She',
 		artist: 'Green Day'
 	},{
+		id: 8,
 		song: 'walking_contradiction',
 		screen: 'Walking Contradiction',
 		artist: 'Green Day'
 	},{
+		id: 9,
 		song: 'welcome_to_paradise',
 		screen: 'Welcome To Paradise',
 		artist: 'Green Day'
 	},{
+		id: 10,
 		song: 'when_i_come_around',
 		screen: 'When I Come Around',
 		artist: 'Green Day'
@@ -59,7 +69,7 @@ $(document).ready(function(){
 	$(document).on('click', '.options-buttons', function(){
 		var audioDiv = $('<div>');
 		audioDiv.addClass('song-playing');
-		var currentSongDiv = $('<div class="current-song-info">')
+		var currentSongDiv = $('<div class="current-song-info">');
 		switch($(this).val()){
 			case "start":
 				appendAudioTags(randomSong, audioDiv);
@@ -113,15 +123,33 @@ $(document).ready(function(){
 		$('#playing-' + songs[index].song)[0].pause();
 	}
 
+	var instructionsP = $('<p>')
+	var instructionsH3 = $('<h3>');
+	instructionsH3.text('Click on Song to Play')
+	instructionsP.append(instructionsH3);
+	$('div').eq(0).append(instructionsP)
+
 	var songListDiv = $('<div>');
 	var songList = $('<ol>');
 	var songLi;
 	for(var i = 0; i < songs.length; i++){
 		songLi = $('<li>')
-		songLi.text('Song: ' + songs[i].screen + ", Artist: " + songs[i].artist);
+		songLi.html('Song: <button class="direct-play" data-id="' + songs[i].id + '">' + songs[i].screen + "</button>, Artist: " + songs[i].artist);
 		songList.append(songLi);
 	}
 	songListDiv.append(songList);
-	$('div').eq(0).append(songListDiv)
+	$('div').eq(0).append(songListDiv);
+
+	$(document).on('click', '.direct-play', function(){
+		$('.song-playing').remove();
+		var audioDiv = $('<div>');
+		audioDiv.addClass('song-playing');
+		var currentSongDiv = $('<div class="current-song-info">');
+
+		var song = $(this).data('id') - 1;
+		randomSong = song;
+		appendAudioTags(song, audioDiv);
+		appendPTags(song, currentSongDiv, audioDiv);
+	})
 
 })
